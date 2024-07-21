1db4b45d448ecb3ceda621398d23ed066e7894e5
artwaw <junk@trollnet.com.pl>
2024-07-21 22:52:01 +0100

    Added legal

---
+++ README.md
@@ -0,0 +1,15 @@
+# GemBrowser
+Small and simple Gemini protocol. Crossplatform.
+
+Build instructions, releases, wiki etc. will be added/updated in due time.
+It compiles and essentially works however is in early alpha state - many features are not yet implemented, especially user-facing ones.
+
+#Uses:
+ - [Qt 6](https://qt.io/) for native widgets front end.
+ - [OpenSSL](https://openssl.org/) for TLS 
+
+Implementation based on document [Gemini network protocol specification ver. 0.24.0](https://geminiprotocol.net/docs/protocol-specification.gmi)
+
+[(c)2024 Artur Wawrowski (t/a TrollNet)](https://trollnet.com.pl/)
+
+[Written and maintained by Artur Wawrowski](mailto:junk@trollnet.com.pl)
--- src/Desktop/gembrowser.h
+++ src/Desktop/gembrowser.h
@@ -1,3 +1,18 @@
+/*
+This file is part of GemBrowser project.
+GemBrowser is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+GemBrowser is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details (file LICENSE).
+You should have received a copy of the GNU General Public License
+along with GemBrowser.  If not, see <https://www.gnu.org/licenses/>.
+SPDX: GPL-3.0-or-later
+*/
+
 #ifndef GEMBROWSER_H
 #define GEMBROWSER_H
 
